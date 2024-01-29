e70e6dda0464baf9cbaecdc2284620384bc009a3
Alfredo <146830711+JockerAG@users.noreply.github.com>
2024-01-29 20:48:59 +0000

    contact-list en proceso

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,20 +2,35 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+				{title: "FIRST", background: "white", initial: "white"},
+				{title: "SECOND", background: "white", initial: "white"}],
+
+			titulo: 'Flux con React',
+			myArray: [],
+			myObject: {},
+			isLogin: false,
+			cohorte: 'Spain 54',
+			favorites: [],
+			users:[]
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
+			getUsers: async () =>  {
+				const url = 'https://playground.4geeks.com/apis/fake/contact/agenda/Agenda-Alfredo';
+				const options = {
+					method: "GET"
+				};
+				const response = await fetch(url, options);
+				if (!response.ok) {
+					console.log("Error en el fetch", response.status, response.statusText)
+					return response.status
+				}
+				const data = await response.json()
+				console.log(data);
+				// Codigo con la logica necesaria 
+				setStore( {users: data })
+			},
+
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
